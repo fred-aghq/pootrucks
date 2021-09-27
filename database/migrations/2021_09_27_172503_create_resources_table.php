7d914b113251581658e17c6278b931bbd3982bd7
Fred Spooner <fred@aghq.uk>
2021-09-28 00:48:30 +0100

    test: remove MissionStoreTest, implement basic expectations for storing resource data

---
+++ database/migrations/2021_09_27_172503_create_resources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->timestamps();
+            $table->string('resource_type_id')->index();
+            $table->string('location');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+}
---
+++ tests/Feature/ResourceStoreTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ResourceType;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Util\Test;
+use Tests\TestCase;
+use Tests\TestConcerns\PostsXML;
+
+class ResourceStoreTest extends TestCase
+{
+    use PostsXML, RefreshDatabase;
+    public function test_it_stores_successfully_with_valid_data()
+    {
+        $res = $this->json('POST', route('resources.store'), [
+            'type' => 'corn',
+            'amount' => 1234,
+            'location' => 'Des Moines, Iowa',
+        ]);
+
+        $res->assertOk();
+
+        $this->assertDatabaseHas('resources', [
+            'amount' => 1234,
+            'location' => 'Des Moines, Iowa',
+            'resource_type_id' => ResourceType::first()->id,
+        ]);
+    }
+}
